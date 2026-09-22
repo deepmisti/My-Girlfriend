85907745a2de88f3cf5ad91544eec5b98c5b476b
deepmisti <deepsrock.agra@gmail.com>
2026-09-22 13:46:49 +0530

    Update MainActivity.kt

--- app/src/main/java/com/example/mygirlfriend/MainActivity.kt
+++ app/src/main/java/com/example/mygirlfriend/MainActivity.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import com.example.mygirlfriend.R
 
 class MainActivity : Activity() {
 
