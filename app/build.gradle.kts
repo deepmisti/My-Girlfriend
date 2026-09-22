ff6cccb57e4e7ffcd52e880620d868b5d5b6e857
deepmisti <deepsrock.agra@gmail.com>
2026-09-22 08:19:11 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,7 +48,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
 
-    // Compose
+    // Compose dependencies
     implementation("androidx.compose.ui:ui:1.6.2")
     implementation("androidx.compose.ui:ui-graphics:1.6.2")
     implementation("androidx.compose.ui:ui-tooling-preview:1.6.2")
